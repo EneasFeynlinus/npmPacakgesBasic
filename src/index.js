b3488e86e92f92c7a645715a697e8ef17949f991
Eneas Junior <eneas.silva.jr@gmail.com>
2024-11-27 18:45:29 -0300

    Installing log-Symbols dependency to test a dev-dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import chalk from 'chalk';
+import logSymbols from "log-symbols"
 
-console.log(chalk.blue.bgGreen.italic("Olá, eu sou"))
-console.log(chalk.green.bgCyan.bold("Enéas"))
+// console.log(chalk.blue.bgGreen.italic("Olá, eu sou"))
+// console.log(chalk.green.bgCyan.bold("Enéas"))
+
+console.log(logSymbols.success, chalk.green.italic("Servidor iniciado"));
+
+console.log(logSymbols.error, chalk.red.bgGray.italic("Servidor com problema"))
